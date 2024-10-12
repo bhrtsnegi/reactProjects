90f7e1e9c756232aa7eb490a69e2a4adb83be579
bhrtsnegi <1bhrtsnegi@gmail.com>
2024-10-12 19:59:13 +0530

    update: app.jsx with basic layout

--- BMICalc/src/App.jsx
+++ BMICalc/src/App.jsx
@@ -1,6 +1,20 @@
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <div>
+        <h2>BMI Calculator</h2>
+        <form>
+          <div>
+            <label>Weight(lbs)</label>
+            <input type="text" placeholder="Enter Your Weight" value={weight} onChange={(e)=>{weight}}/>
+          </div>
+          <div>
+            <label>Height(lbs)</label>
+            <input type="text" placeholder="Enter Your Height" value={height} onChange={(e)=>{height}}/>
+          </div>
+        </form>
+      </div>
+    </div>
   )
 }
 
