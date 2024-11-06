15f38a4a2d00556c898101288083e0912479f236
bhrtsnegi <1bhrtsnegi@gmail.com>
2024-11-06 08:41:07 +0530

    add basic boilerplate

---
+++ toDoApp/src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div className="text-center">
+      <form>
+        <input type="text" className="m-8 w-[25%] border border-gray-700 rounded-lg" />
+      </form>
+    </div>
+  )
+}
+
+export default App
